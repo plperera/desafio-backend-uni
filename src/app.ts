8fd37f91c86af3118ddcf2902dd20d16cb0cced4
Pedro Pereira <plp.leoncio@gmail.com>
2024-04-08 17:23:40 -0300

    feat: implementação de algumas libs simples

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import { loadEnv } from "@/config";
 import cors from "cors";
-import express, { Express } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import createError from "http-errors";
 
 import "express-async-errors";
 import { productRouter } from "@/routers";
@@ -8,12 +11,27 @@ import { clientRouter } from "./routers/client-router";
 
 loadEnv();
 
-const app = express();
+const app: Express = express();
 app
   .use(cors())
   .use(express.json())
+  .use(cookieParser())
+  .use(morgan("dev"))
   .get("/health", (_req, res) => res.send("OK!"))
   .use("/produtos", productRouter)
   .use("/clientes", clientRouter)
+  .use((_req, _res, next) => {
+    next(createError(404));
+  });
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
 
 export default app;
